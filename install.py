7ee2a65f808539d5ad27063939d832c002f51b28
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-17 15:58:37 +0200

    installation de tous les logiciels voulus en 1 seule ligne

--- install.py
+++ install.py
@@ -8,23 +8,22 @@ softList = yaml.load(stream)
 process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 process.stdin.write('module load EasyBuild\n')
 
-for software in softList['softwares']:
-    process.stdin.write('eb ' + software + ' -D --robot\n')
-    process.stdin.write('echo $?\n')
-    out = ""
-    while True:
-        out = process.stdout.readline()
-        try:
-            i = int(out)
-        except ValueError:
-            i = -1
-        if i < 0:
-            sys.stdout.write(out)
+process.stdin.write('eb ' + ' '.join(softList['softwares']) + ' -D --robot\n')
+process.stdin.write('echo $?\n')
+out = ""
+while True:
+    out = process.stdout.readline()
+    try:
+        i = int(out)
+    except ValueError:
+        i = -1
+    if i < 0:
+        sys.stdout.write(out)
+    else:
+        if i == 0:
+            sys.stdout.write('Operation successful\n')
         else:
-            if i == 0:
-                sys.stdout.write('Operation successful\n')
-            else:
-                sys.stdout.write('Operation failed with return code ' + out)
-            break
+            sys.stdout.write('Operation failed with return code ' + out)
+        break
 
 sys.stdout.write("Finishing EasyBuild software installation step.\n")
