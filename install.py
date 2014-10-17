b4ce5c10fb422e9154e5364f388a3486c4033d32
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-17 15:08:23 +0200

    script python installe automatiquement les logiciels avec EasyBuild

--- install.py
+++ install.py
@@ -1,8 +1,30 @@
-import yaml
+import sys
 import subprocess
+import yaml
 
-stream = file('modules.yaml', 'r')
-mouduleList = yaml.load(stream)
+stream = file('softwares.yaml', 'r')
+softList = yaml.load(stream)
 
-for module in moduleList:
-    subprocess.call("eb" + module, shell=True)
+process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+process.stdin.write('module load EasyBuild\n')
+
+for software in softList['softwares']:
+    process.stdin.write('eb ' + software + ' -D --robot\n')
+    process.stdin.write('echo $?\n')
+    out = ""
+    while True:
+        out = process.stdout.readline()
+        try:
+            i = int(out)
+        except ValueError:
+            i = -1
+        if i < 0:
+            sys.stdout.write(out)
+        else:
+            if i == 0:
+                sys.stdout.write('Operation successful\n')
+            else:
+                sys.stdout.write('Operation failed with return code ' + out)
+            break
+
+sys.stdout.write("Finishing EasyBuild software installation step.\n")
