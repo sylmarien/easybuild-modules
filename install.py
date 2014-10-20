6d621ed008559b8dc23ff8d35c56441c1de7ff58
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-20 10:49:40 +0200

    adding the possibility to use any name for the software sets

--- install.py
+++ install.py
@@ -7,7 +7,7 @@ def helper():
 """Usage of this script:
 python install.py [software set] [EasyBuild version]
 
-- The [software set] argument is mandatory and to be chosen between core or experimental.
+- The [software set] argument is mandatory and to be chosen among the software sets you defined in your YAML file. (At Uni.lu: core and experimental)
 - The [EasyBuild version] argument is optional, and the format is simply the three digit version number of the EasyBuild version you want to use. (e.g. 1.15.2 to use EasyBuild version 1.15.2).
 If not specified, the script will use the version chosen by default by your module command.
 
@@ -18,15 +18,22 @@ python install.py core 1.15.2
 
 if len(sys.argv) >= 2 and len(sys.argv) <= 3:
     softwareSet = sys.argv[1]
-    if softwareSet == "core" or softwareSet == "experimental":
-        EBversion = '' if len(sys.argv) == 2 else '/' + sys.argv[2]
+    isPresent = False
+    EBversion = '' if len(sys.argv) == 2 else '/' + sys.argv[2]
 
-        stream = file('softwares.yaml', 'r')
-        softList = yaml.load(stream)
+    stream = file('softwares.yaml', 'r')
+    softList = yaml.load(stream)
 
-        process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        process.stdin.write('module load EasyBuild' + EBversion + '\n')
+    process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process.stdin.write('module load EasyBuild' + EBversion + '\n')
     
+    # We check that the software set exists in the YAML file.
+    for k in softList.iteritems():
+        if softwareSet in k:
+            isPresent = True
+
+    # If it actually exist, we install the listed software.
+    if isPresent:
         for software in softList[softwareSet]:
             process.stdin.write('eb ' + software + ' -D --robot\n')
             process.stdin.write('echo $?\n')
@@ -47,7 +54,7 @@ if len(sys.argv) >= 2 and len(sys.argv) <= 3:
                     break
 
         sys.stdout.write("Finishing EasyBuild software installation step.\n")
-
+    # If it doesn't, we print an error message as well as an help to use the script.
     else:
         sys.stdout.write("Error: Invalid set of software, please use either core or experimental. (Instead of " + softwareSet  + ")\n")
         helper()
